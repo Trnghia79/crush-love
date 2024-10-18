40f8a2e64731338059303fc582f840e6c44d3c14
Trnghia79 <k62.2311110216@ftu.edu.vn>
2024-10-18 22:46:53 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "From boy khối 1 with luv!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
+    "Nhân dịp 20/10, đồng an team 71 chúng tớ có món quà nho nhỏ tặng chị và các cậu nè 🥰 Mọi người nhận cho chúng tớ vui nhé^^",
   buttonYes: "Tớ đồng ý",
   buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "Đứa nào vừa bấm 0 đồng ý?",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Hellu các cậu nhé, hôm nay là 20/10 đầu tiên mà team 71 chúng mình được ở bên nhau nè 🥰 Qua mùa wib vừa rồi, mình đã cùng nhau trải qua vô vàn các cung bậc cảm xúc và những kỉ niệm đáng nhớ. 1 trong số đó là gặp được mọi người - 18 cô gái đặc biệt của chúng tớ. </br> Chúng em/tớ cảm ơn chị và các cậu vì đã luôn là hậu phương vững chắc, là những người bạn quý giá của chúng em. Nhân dịp 20/10 này, chúng tớ chúc mọi người sẽ luôn xinh đẹp, giỏi giang, thật nhiều sức khỏe, đạt hobo, full A, 100 đrl, 0 bị qha hoặc ngkhac dí. Chúc cậu cậu 8386 mãi đỉnh nhé.  </br> Chúng tớ còn 1 món quà nho nhỏ nữa, mọi người ấn vào link này để nhận nha",
 };
